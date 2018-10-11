59a9edb50c78ddb20437806406fc27d085618ee7
Jonathon Fry <jonfry22@gmail.com>
2018-10-11 14:30:44 +0100

    Testing gatsby site

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/jonathonfry/Documents/Code/iamjonfry/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/jonathonfry/Documents/Code/iamjonfry/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
