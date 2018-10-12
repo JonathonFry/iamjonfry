6ec13f6c3ae5fc38c803ed8202d2bd3e33866fa8
Jonathon Fry <jonfry22@gmail.com>
2018-10-12 16:16:25 +0100

    Fix page and posts types

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,17 +8,19 @@ exports.createPages = ({ graphql, actions }) => {
 
   return new Promise((resolve, reject) => {
     const blogPost = path.resolve("./src/templates/blog-post.js");
+    const page = path.resolve("./src/templates/page.js");
+
     resolve(
       graphql(
         `
           {
-            allMarkdownRemark( 
-              sort: { fields: [frontmatter___date], order: DESC },
-              filter: { fileAbsolutePath: {regex : "\/post/"} },
+            allMarkdownRemark(
+              sort: { fields: [frontmatter___date], order: DESC }
               limit: 1000
             ) {
               edges {
                 node {
+                  fileAbsolutePath
                   fields {
                     slug
                   }
@@ -36,7 +38,6 @@ exports.createPages = ({ graphql, actions }) => {
           reject(result.errors);
         }
 
-        // Create blog posts pages.
         const posts = result.data.allMarkdownRemark.edges;
 
         _.each(posts, (post, index) => {
@@ -44,64 +45,15 @@ exports.createPages = ({ graphql, actions }) => {
             index === posts.length - 1 ? null : posts[index + 1].node;
           const next = index === 0 ? null : posts[index - 1].node;
 
+          let component = post.node.fileAbsolutePath.includes('/post/') ? blogPost : page;
+
           createPage({
             path: post.node.fields.slug,
-            component: blogPost,
+            component: component,
             context: {
               slug: post.node.fields.slug,
               previous,
               next
-            }
-          });
-        });
-      })
-    );
-  });
-};
-
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-
-  return new Promise((resolve, reject) => {
-    const page = path.resolve("./src/templates/page.js");
-    resolve(
-      graphql(
-        `
-          {
-            allMarkdownRemark( 
-              sort: { fields: [frontmatter___date], order: DESC },
-              filter: { fileAbsolutePath: {regex : "\/page/"} },
-              limit: 1000
-            ) {
-              edges {
-                node {
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    title
-                  }
-                }
-              }
-            }
-          }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors);
-          reject(result.errors);
-        }
-
-        // Create pages.
-        const posts = result.data.allMarkdownRemark.edges;
-
-        _.each(posts, (post, index) => {
-
-          createPage({
-            path: post.node.fields.slug,
-            component: page,
-            context: {
-              slug: post.node.fields.slug,
             }
           });
         });
