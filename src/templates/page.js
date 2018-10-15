d72f8514069d7c991da01bf7f12001ca4350cb4d
Jonathon Fry <jonfry22@gmail.com>
2018-10-15 08:21:37 +0100

    Remove next/prev from page

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 import get from "lodash/get";
 
-import Bio from "../components/bio";
 import Layout from "../components/layout";
 import { rhythm, scale } from "../utils/typography";
 
@@ -12,7 +11,6 @@ class PageTemplate extends React.Component {
     const post = this.props.data.markdownRemark;
     const siteTitle = get(this.props, "data.site.siteMetadata.title");
     const siteDescription = post.excerpt;
-    const { previous, next } = this.props.pageContext;
 
     return (
       <Layout location={this.props.location}>
@@ -26,46 +24,22 @@ class PageTemplate extends React.Component {
           style={{
             ...scale(-1 / 5),
             display: "block",
-            marginBottom: rhythm(1),
+            marginBottom: rhythm(1)
           }}
         >
           {post.frontmatter.date}
         </p>
-        <div style={{
-            maxWidth: rhythm(24),
+        <div
+          style={{
+            maxWidth: rhythm(24)
           }}
-          dangerouslySetInnerHTML={{ __html: post.html }} />
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
         <hr
           style={{
             marginBottom: rhythm(1)
           }}
         />
-        <Bio />
-
-        <ul
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-between",
-            listStyle: "none",
-            padding: 0
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
       </Layout>
     );
   }
